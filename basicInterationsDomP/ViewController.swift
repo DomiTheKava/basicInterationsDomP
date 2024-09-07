a2ca63b2021d836692f87599855bb4c6cf0a4012
DomiTheKava <Dominikpenkava@yahoo.com>
2024-09-07 16:29:27 -0500

    V1 9/7/24

--- basicInterationsDomP/ViewController.swift
+++ basicInterationsDomP/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var btn: UIButton!
+    @IBOutlet weak var txtEntered: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeBtn(_ sender: UIButton) {
+        label.text = txtEntered.text?.uppercased() ?? "no text entered"
+        UIView.animateKeyframes(withDuration: 1, delay: 0, options: [], animations: {
+         
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.5, animations: {
+                self.txtEntered.transform = CGAffineTransform(translationX: 0, y: -100)
+            })
+            
+            UIView.addKeyframe(withRelativeStartTime: 0.5, relativeDuration: 0.5, animations: {
+                self.txtEntered.transform = CGAffineTransform.identity
+            })
+            
+        })
+    }
+    
 }
 
